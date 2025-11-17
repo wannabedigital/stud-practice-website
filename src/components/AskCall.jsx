149b29ef19d3b23a675805fa99c9a29518af67c7
wannabedigital <digitaldxmmy@gmail.com>
2025-11-18 01:29:54 +0300

    Modal with feedback form done

--- src/components/AskCall.jsx
+++ src/components/AskCall.jsx
@@ -1,43 +1,57 @@
 import Image from 'next/image';
-import Link from 'next/link';
+
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 
 import styles from '@styles/askCall.module.css';
 import { DividerDesktop, DividerMobile } from '@components/Dividers';
 import { CONTACTS } from '@config/contacts.config';
-import { PAGES } from '@config/pages.config';
+import FeedbackModal from './FeedbackModal';
 
 const AskCall = ({ title, subTitle }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <section className={styles.homeCallSection}>
-      <div className={styles.homeCallBackground}>
-        <Image src={'/img/home/call_bg.svg'} alt="section bg" fill priority />
-      </div>
-      <div
-        className={`${styles.contentContainer} ${styles.homeCallSectionContent}`}
-      >
-        <h1 className={styles.sectionTitle}>{title}</h1>
+    <>
+      <section className={styles.homeCallSection}>
+        <div className={styles.homeCallBackground}>
+          <Image src={'/img/home/call_bg.svg'} alt="section bg" fill priority />
+        </div>
+        <div
+          className={`${styles.contentContainer} ${styles.homeCallSectionContent}`}
+        >
+          <h1 className={styles.sectionTitle}>{title}</h1>
 
-        <div className={styles.desktopDivider}>
-          <DividerDesktop color={'#304B73'} />
-        </div>
-        <div className={styles.mobileDivider}>
-          <DividerMobile color={'#304B73'} />
-        </div>
+          <div className={styles.desktopDivider}>
+            <DividerDesktop color={'#304B73'} />
+          </div>
+          <div className={styles.mobileDivider}>
+            <DividerMobile color={'#304B73'} />
+          </div>
 
-        <div className={styles.sectionText}>
-          <p>{subTitle}</p>
+          <div className={styles.sectionText}>
+            <p>{subTitle}</p>
+          </div>
+          <div className={styles.callConteiner}>
+            <a href={CONTACTS.PHONE.link} className={styles.callButton}>
+              <span>{CONTACTS.PHONE.visible}</span>
+              <span className={styles.callButtonFlare} />
+            </a>
+            <button
+              onClick={() => setShowModal(true)}
+              className={styles.askForCall}
+            >
+              или закажите звонок
+            </button>
+          </div>
         </div>
-        <div className={styles.callConteiner}>
-          <a href={CONTACTS.PHONE.link} className={styles.callButton}>
-            <span>{CONTACTS.PHONE.visible}</span>
-            <span className={styles.callButtonFlare} />
-          </a>
-          <Link href={PAGES.CONTACTS.path} className={styles.askForCall}>
-            или закажите звонок
-          </Link>
-        </div>
-      </div>
-    </section>
+      </section>
+      {showModal &&
+        createPortal(
+          <FeedbackModal onClose={() => setShowModal(false)} />,
+          document.body
+        )}
+    </>
   );
 };
 
---
+++ src/components/FeedbackModal.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useRef } from 'react';
+
+import FeedbackForm from '@components/FeedbackForm';
+import styles from '@styles/feedbackModal.module.css';
+
+const CloseIcon = () => {
+  return (
+    <div className={styles.closeIconWrapper}>
+      <svg
+        width="800"
+        height="800"
+        viewBox="0 0 800 800"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_384_8)">
+          <path
+            d="M277.653 277.653L523.346 523.346"
+            stroke="#f9f9f9"
+            strokeWidth="66.6667"
+            strokeLinecap="round"
+          />
+          <path
+            d="M523.346 277.653L277.653 523.346"
+            stroke="#f9f9f9"
+            strokeWidth="66.6667"
+            strokeLinecap="round"
+          />
+          <circle cx="400" cy="400" r="380" stroke="#f9f9f9" strokeWidth="40" />
+        </g>
+        <defs>
+          <clipPath id="clip0_384_8">
+            <rect width="800" height="800" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+};
+
+const FeedbackModal = ({ onClose }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleEscape = e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleEscape);
+
+    return () => window.removeEventListener('keydown', handleEscape);
+  }, [onClose]);
+
+  const handleOverlayClick = e => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className={styles.feedbackPortal} onMouseDown={handleOverlayClick}>
+      <div className={styles.portalContentContainer} ref={modalRef}>
+        <div className={styles.modalHeader}>
+          <h2 className={styles.modalTitle}>
+            Заполните форму чтобы оставить заявку
+          </h2>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.modalBody}>
+          <div className={styles.closeButtonWrapper}>
+            <button className={styles.closeButton} onClick={onClose}>
+              <CloseIcon />
+            </button>
+          </div>
+          <FeedbackForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackModal;
